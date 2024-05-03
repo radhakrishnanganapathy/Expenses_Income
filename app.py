6734514aff03d8931bb2363365c7e17618a72770
radhakrishnanr <radhakrishnanganapathyr@gmail.com>
2024-05-03 10:09:28 +0530

    db changes

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import psycopg2 as psc
 from datetime import datetime
+from psycopg2 import extensions
 
 
 db_params = {
@@ -11,7 +12,8 @@ db_params = {
     "port": "5432"  # Default PostgreSQL port is 5432
 }
 # db_params = " postgresql://radhakrishnan:weJyKwjSj60UNzr4MXIEZ79FV7sf2ZHY@dpg-coiuro5jm4es739v1hvg-a.oregon-postgres.render.com/rkrish_db"
-conn = psc.connect(db_params)
+dsn = extensions.make_dsn(**db_params)
+conn = psc.connect(dsn)
 cursor = conn.cursor()
 
 st.header("Income and Expenses Manager")
