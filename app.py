29be646d341d37134c982431e29de66f73f57bee
radhakrishnanr <radhakrishnanganapathyr@gmail.com>
2024-05-03 10:04:45 +0530

    db changes

--- app.py
+++ app.py
@@ -3,7 +3,14 @@ import psycopg2 as psc
 from datetime import datetime
 
 
-db_params = " postgresql://radhakrishnan:weJyKwjSj60UNzr4MXIEZ79FV7sf2ZHY@dpg-coiuro5jm4es739v1hvg-a.oregon-postgres.render.com/rkrish_db"
+db_params = {
+    "dbname": "rkrish_db",
+    "user": "radhakrishnan",
+    "password": "weJyKwjSj60UNzr4MXIEZ79FV7sf2ZHY",
+    "host": "dpg-coiuro5jm4es739v1hvg-a.oregon-postgres.render.com",
+    "port": "5432"  # Default PostgreSQL port is 5432
+}
+# db_params = " postgresql://radhakrishnan:weJyKwjSj60UNzr4MXIEZ79FV7sf2ZHY@dpg-coiuro5jm4es739v1hvg-a.oregon-postgres.render.com/rkrish_db"
 conn = psc.connect(db_params)
 cursor = conn.cursor()
 
